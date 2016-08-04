ca5b9d21c9fa511c7f27c85240dfbac58d4414cb
jjkeeeem <jjkeeeem@gmail.com>
2016-08-04 12:22:45 +0000

    users 컨트롤러, account_setting,profile 모델 추가

---
+++ app/models/account_setting.rb
@@ -0,0 +1,5 @@
+class AccountSetting < ActiveRecord::Base
+    
+    belongs_to :user
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731165153) do
+ActiveRecord::Schema.define(version: 20160804120055) do
+
+  create_table "account_settings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "forms", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +42,11 @@ ActiveRecord::Schema.define(version: 20160731165153) do
     t.string   "image_url",  default: ""
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
