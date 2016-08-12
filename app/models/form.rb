fdec69cac199fd9ac28c5902672cb3d6cdfbdd05
wltn716 <wltn716@naver.com>
2016-08-12 04:25:52 +0000

    해시태그 부분수정

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,6 +2,6 @@ class Form < ActiveRecord::Base
   has_many :freplies
   is_impressionable
   def self.search(search)
-      where("title Like ? OR content LIKE ? ", "%#{search}%", "%#{search}%")
+      where("title Like ? OR content LIKE ? OR hashtag LIKE ? ", "%#{search}%", "%#{search}%", "%#{search}%")
   end
 end
