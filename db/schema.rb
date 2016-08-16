3d0f91d017ed125d40a3ffdc19e01f1269fbfcec
canbecamby <silverplay01@naver.com>
2016-08-16 10:39:11 +0000

    칼럼 추가해서 회원가입 오류 수정

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808122911) do
+ActiveRecord::Schema.define(version: 20160816103337) do
 
   create_table "account_settings", force: :cascade do |t|
     t.datetime "created_at", null: false
