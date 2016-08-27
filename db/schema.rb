63a204b206d5178daec40ffa6281c792e19aaa7b
wltn716 <wltn716@naver.com>
2016-08-27 07:44:32 +0000

    수정

--- db/schema.rb
+++ db/schema.rb
@@ -112,10 +112,6 @@ ActiveRecord::Schema.define(version: 20160823104624) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "major"
@@ -124,7 +120,6 @@ ActiveRecord::Schema.define(version: 20160823104624) do
     t.string   "provider"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
